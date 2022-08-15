45bcedcbfa14930494acdb778445816f030bfb40
ccp92 <mail@chrisparsons.dev>
2022-08-15 21:30:21 +0100

    Switch to checkbox rather than Yes/No message

--- components/pokemon_details/index.tsx
+++ components/pokemon_details/index.tsx
@@ -7,7 +7,7 @@ const PokemonDetails = (props: PokemonDetailsProps) => {
   return (
     <div>
       <h1>{props.name}</h1>
-      <p>Owned: {props.owned ? "Yes" : "No"}</p>
+      <input type="checkbox" checked={props.owned} />
     </div>
   );
 };
